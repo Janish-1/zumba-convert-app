d565821180aa17955bff1b9c1bcababee7dd757b
Janish-1 <janish.pancholi11@gmail.com>
2024-03-05 15:05:42 +0530

    settings for converted app

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView source={{ uri: 'http://zumba.pujanpaath.com' }} style={{ flex: 1 }} />;
     </View>
   );
 }
